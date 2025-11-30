7ba4d07221e8eecd633a948691f2832e87ecbf6f
Sandeep Dubey <sandeepdubey0000@gmail.com>
2025-11-30 14:29:02 +0530

    Delete docs directory

--- docs/notebook_outline.py
+++
@@ -1,6 +0,0 @@
-
-# Tesla Procurement NPI - Notebook Outline
-# 1. Load RFQ dataset
-# 2. Calculate negotiated savings and supplier score
-# 3. Build negotiation simulation (what-if)
-# 4. Export summary tables for Power BI
